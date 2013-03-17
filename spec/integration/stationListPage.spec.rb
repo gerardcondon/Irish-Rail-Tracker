7f344221cb51696bce92a92ce512f7afc2d65faf
Gerard Condon <gercondon+github@gmail.com>
2013-03-17 14:05:44 +0000

    fix capybara tests

--- spec/integration/stationListPage.spec.rb
+++ spec/integration/stationListPage.spec.rb
@@ -11,13 +11,14 @@ describe "Station List Page Layout", js: true, type: :feature do
     it "should have four parts" do
         visit "/"
         find("#nav-bar-station-list-button").click
-        all('accordion-group').length.should eq 4
+        sleep(4)
+        page.all('.accordion-group').length.should eq 4
     end
 
     it "should have a section for Dart trains" do
         visit "/"
         find("#nav-bar-station-list-button").click
-        page.should have_selector("station-list-dart-accordion-group")
+        page.should have_selector("#station-accordion-group-D")
     end
   end
 end
